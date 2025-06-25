66cababff962cc364a4e5e7257b13bb71564cd1c
ScarlettRed6 <jcaamrelo@gmail.com>
2025-06-26 00:03:18 +0800

    changed theme to cyberpunk dark from samueljoli

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,25 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
+  -- gruvbox theme
+  -- { "ellisonleao/gruvbox.nvim" },
+
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
+
+  -- cyberpunk theme
 
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    "samueljoli/cyberpunk.nvim",
+    name = "cyberpunk",
+    priority = 1000, -- make sure it loads before LazyVim UI
+    config = function()
+      require("cyberpunk").setup({
+        theme = "dark", -- or "light" if available
+      })
+      --vim.cmd.colorscheme("cyberpunk")
+    end,
   },
 }
