aa82a82aaac537b34cfc1fe3418594145c746b4e
ScarlettRed6 <jcaamrelo@gmail.com>
2025-06-26 13:28:51 +0800

    updated neovim theme to gruvbox

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,25 +1,24 @@
 return {
   -- gruvbox theme
-  -- { "ellisonleao/gruvbox.nvim" },
-
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "gruvbox",
-  --   },
-  -- },
-
-  -- cyberpunk theme
+  { "ellisonleao/gruvbox.nvim" },
 
   {
-    "samueljoli/cyberpunk.nvim",
-    name = "cyberpunk",
-    priority = 1000, -- make sure it loads before LazyVim UI
-    config = function()
-      require("cyberpunk").setup({
-        theme = "dark", -- or "light" if available
-      })
-      --vim.cmd.colorscheme("cyberpunk")
-    end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
   },
+
+  -- cyberpunk theme
+  --{
+  --  "samueljoli/cyberpunk.nvim",
+  --  name = "cyberpunk",
+  --  priority = 1000, -- make sure it loads before LazyVim UI
+  --  config = function()
+  --    require("cyberpunk").setup({
+  --      theme = "dark", -- or "light" if available
+  --    })
+  --    --vim.cmd.colorscheme("cyberpunk")
+  --  end,
+  --},
 }
