9f5214887c0531d03557cd2490fc11d426be6d26
ScarlettRed6 <jcaamrelo@gmail.com>
2025-06-26 11:49:51 +0800

    fixed my bluetooth toggle

--- config/polybar-custom/poly-scripts/bluetooth-toggle.sh
+++ config/polybar-custom/poly-scripts/bluetooth-toggle.sh
@@ -6,16 +6,16 @@
 status=$(bluetoothctl show | grep "Powered" | awk '{print $2}')
 
 if [ "$1" = "toggle" ]; then
-	if [ "$status" = "yes" ]; then
-		bluetoothctl power off
-	else
-		bluetoothctl power on
-	fi
+  if [ "$status" = "yes" ]; then
+    bluetoothctl power off
+  else
+    bluetoothctl power on
+  fi
 else
-	# Display update icon based on the state
-	if [ "$status" = "yes" ]; then
-		echo "%{F#FF0000}%{T1}%{F-} On%{T-}"
-	else
-		echo "%{F#FF0000}%{T1}%{F-} Off%{T-}"
-	fi
+  # Display update icon based on the state
+  if [ "$status" = "yes" ]; then
+    echo "%{F#FF0000}%{T1}%{F-} On%{T-}"
+  else
+    echo "%{F#FF0000}%{T1}%{F-} Off%{T-}"
+  fi
 fi
